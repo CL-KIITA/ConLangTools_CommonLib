8b5965ef91a85ad04b22a655c13ea835645e66e6
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-10-26 23:48:03 +0900

    add: 辞書フォーマット関連

---
+++ lib/dicformat/convert.dart
@@ -0,0 +1,1 @@
+library "dicformat_convert";
---
+++ lib/dicformat/formats.dart
@@ -0,0 +1,11 @@
+library "dicformat_formats";
+
+import "../src/aldc.dart";
+import "../src/asd.dart";
+import "../src/otm_json.dart";
+import "../src/pdic.dart";
+
+export "../src/aldc.dart";
+export "../src/asd.dart";
+export "../src/otm_json.dart";
+export "../src/pdic.dart";
