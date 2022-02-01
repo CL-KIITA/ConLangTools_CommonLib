8546302e2c2b4a50735786ced5e7551f38f90850
halka9000stg <csta19097@g.nihon-u.ac.jp>
2022-02-01 11:15:58 +0900

    add: ユーフォニー指数、ジニ係数指数などのためのファイル

---
+++ lib/index/lang.dart
@@ -0,0 +1,9 @@
+/**
+ Contains some Language Indexes which measure naturalness, artificialness, and (arbitrary) X-familyness, such as the Euphony index
+*/
+library lang_index;
+
+import "package:conlang_tools_common_lib/src/mathlib.dart";
+
+class Euphony1{}
+class Euphony1_2{}
